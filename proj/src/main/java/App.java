395c43c40aa75c490279450b57484b7126973dd9
Sridhar Sarnobat <srsarnob@cisco.com>
2020-03-21 23:11:06 -0700

    Minor changes (untested)

--- proj/src/main/java/App.java
+++ proj/src/main/java/App.java
@@ -1,9 +1,13 @@
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.file.FileAlreadyExistsException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import net.fusejna.DirectoryFiller;
 import net.fusejna.ErrorCodes;
@@ -26,15 +30,98 @@ public class App extends FuseFilesystemAdapterFull {
             String string = "family_tree";
             String string2 = "/Users/srsarnob/github/fuse-java/proj/" + string;
             new ProcessBuilder().command("diskutil", "unmount", string2).inheritIO().start();
-            
+
             try {
                 Files.createDirectory(Paths.get(string2));
             } catch (FileAlreadyExistsException e) {
 
             }
+
+            new Thread() {
+
+                @Override
+                public void run() {
+                    try {
+                        File myObj = new File("/Users/srsarnob/sarnobat.git/gedcom/rohidekar.ged");
+                        Scanner myReader = new Scanner(myObj);
+                        Individual individual = null;
+                        while (myReader.hasNextLine()) {
+                            String data = myReader.nextLine();
+//                            System.out.println("SRIDHAR App.main(...).new Thread() {...}.run() " + data);
+                            if (data.startsWith("0") && data.endsWith("INDI")) {
+
+                                if (individual != null) {
+                                    System.out.println(individual.toString());
+                                }
+                                String regex = "0..(.*)..INDI";
+                                Pattern p = Pattern.compile(regex);
+                                Matcher matcher = p.matcher(data);
+                                if (matcher.find()) {
+                                    String s = matcher.group(1);
+//                                    System.out.println("SRIDHAR App.main(...).new Thread() {...}.run() " + s);
+                                    individual = new Individual(s);
+                                } else {
+                                    throw new RuntimeException("Developer error");
+                                }
+                                continue;
+                            }
+                            if (individual == null) {
+                                continue;
+                            }
+                            if (data.startsWith("2 GIVN")) {
+                                String replaceAll = data.replaceAll(".*GIVN ", "");
+                                individual.setFirstName(replaceAll);
+                            } else if (data.startsWith("2 SURN")) {
+                                String replaceAll = data.replaceAll(".*SURN ", "");
+                                individual.setLastName(replaceAll);
+                            }
+                        }
+                        myReader.close();
+                    } catch (FileNotFoundException e) {
+                        System.out.println("An error occurred.");
+                        e.printStackTrace();
+                    }
+
+                }
+
+            }.run();
+            ;
             new App().log(true).mount(string);
 //            System.exit(1);
 //          args = new String[]{ "/sarnobat.garagebandbroken/trash/fuse-jna/mnt" };
+        }
+    }
+
+    private static class Individual {
+        private final String id;
+
+        String firstName;
+
+        Individual(String id) {
+            this.id = id;
+        }
+
+        String getFirstName() {
+            return firstName;
+        }
+
+        void setFirstName(String firstName) {
+            this.firstName = firstName;
+        }
+
+        String getLastName() {
+            return lastName;
+        }
+
+        void setLastName(String lastName) {
+            this.lastName = lastName;
+        }
+
+        String lastName;
+        
+        @Override
+        public String toString() {
+            return firstName + " " + lastName;
         }
     }
 
@@ -55,12 +142,11 @@ public class App extends FuseFilesystemAdapterFull {
             stat.setMode(NodeType.FILE).size(CONTENTS.length());
             return 0;
         }
-        //return -ErrorCodes.ENOENT();
+        // return -ErrorCodes.ENOENT();
     }
 
     @Override
-    public int read(String path, ByteBuffer buffer, long size, long offset,
-            final FileInfoWrapper info) {
+    public int read(String path, ByteBuffer buffer, long size, long offset, final FileInfoWrapper info) {
         // Compute substring that we are being asked to read
         final String fileContents = CONTENTS.substring((int) offset,
                 (int) Math.max(offset, Math.min(CONTENTS.length() - offset, offset + size)));
@@ -75,11 +161,11 @@ public class App extends FuseFilesystemAdapterFull {
         filler.add("sridhar.txt");
         return 0;
     }
-    
+
     @Override
     public int rename(String oldName, String newName) {
-        System.out.println("SRIDHAR App.rename() mv " + oldName +" "+newName);
+        System.out.println("SRIDHAR App.rename() mv " + oldName + " " + newName);
         return 0;
-        
+
     }
 }
