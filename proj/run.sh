6ce8ce92343ad370f4681d2f917b0d972fc84e92
Sridhar Sarnobat <ss401533@gmail.com>
2020-03-22 23:27:02 -0700

    Minor changes (untested)

--- proj/run.sh
+++ proj/run.sh
@@ -1,4 +1,7 @@
 cd /sarnobat.garagebandbroken/Desktop/github-repositories/fuse-java/proj/
+
+mvn install:install-file -DgroupId=net.fusejna -DartifactId=fuse-jna -Dversion=0.0.1-SNAPSHOT  -Dpackaging=jar -Dfile=/sarnobat.garagebandbroken/Desktop/github-repositories/fuse-java/fuse-jna-0.0.1-SNAPSHOT.jar
+
 mvn compile assembly:single
 java -jar target/myproj-1.0-SNAPSHOT-jar-with-dependencies.jar
 
