b2c63b52d42d356008ca96d9e0aa58aa05a094ff
Sridhar Sarnobat <ss401533@gmail.com>
2020-03-22 23:26:00 -0700

    Minor changes (untested)

--- proj/run.sh
+++ proj/run.sh
@@ -1,1 +1,6 @@
-/Library/Java/JavaVirtualMachines/jdk1.7.0_25.jdk/Contents/Home/bin/java -Dfile.encoding=UTF-8 -classpath /sarnobat.garagebandbroken/Desktop/github-repositories/fuse-java/proj/target/classes:/sarnobat.garagebandbroken/.m2/repository/junit/junit/3.8.1/junit-3.8.1.jar:/sarnobat.garagebandbroken/Desktop/github-repositories/fuse-java/fuse-jna-0.0.1-SNAPSHOT.jar:/sarnobat.garagebandbroken/.m2/repository/net/java/dev/jna/jna/3.4.0/jna-3.4.0.jar:/sarnobat.garagebandbroken/.m2/repository/com/google/guava/guava/19.0/guava-19.0.jar:/sarnobat.garagebandbroken/.m2/repository/commons-io/commons-io/2.4/commons-io-2.4.jar App
+cd /sarnobat.garagebandbroken/Desktop/github-repositories/fuse-java/proj/
+mvn compile assembly:single
+java -jar target/myproj-1.0-SNAPSHOT-jar-with-dependencies.jar
+
+# This has to be compiled in Eclipse unfortunately. Fuse-jna is not a maven dependency
+#/Library/Java/JavaVirtualMachines/jdk1.7.0_25.jdk/Contents/Home/bin/java -Dfile.encoding=UTF-8 -classpath /sarnobat.garagebandbroken/Desktop/github-repositories/fuse-java/proj/target/classes:/sarnobat.garagebandbroken/.m2/repository/junit/junit/3.8.1/junit-3.8.1.jar:/sarnobat.garagebandbroken/Desktop/github-repositories/fuse-java/fuse-jna-0.0.1-SNAPSHOT.jar:/sarnobat.garagebandbroken/.m2/repository/net/java/dev/jna/jna/3.4.0/jna-3.4.0.jar:/sarnobat.garagebandbroken/.m2/repository/com/google/guava/guava/19.0/guava-19.0.jar:/sarnobat.garagebandbroken/.m2/repository/commons-io/commons-io/2.4/commons-io-2.4.jar App
