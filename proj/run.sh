cae44420ecc08359b7d891c9fe24adb65a0cad69
Sridhar Sarnobat <ss401533@gmail.com>
2021-02-28 11:14:49 -0800

    Minor changes (untested)

--- proj/run.sh
+++ proj/run.sh
@@ -1,3 +1,5 @@
+Install Macfuse: https://github.com/osxfuse/osxfuse/releases/download/macfuse-4.0.5/macfuse-4.0.5.dmg
+
 cd ~/github/fuse-java/proj/
 
 mvn install:install-file -DgroupId=net.fusejna -DartifactId=fuse-jna -Dversion=0.0.1-SNAPSHOT  -Dpackaging=jar -Dfile=$HOME/github/fuse-java/fuse-jna-0.0.1-SNAPSHOT.jar
