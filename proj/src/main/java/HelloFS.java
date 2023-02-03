fdd804000f39e52330453235f0007891c0ce8a1e
Sridhar Sarnobat <ss401533@gmail.com>
2023-02-02 21:54:11 -0800

    Minor changes (untested)

--- proj/src/main/java/HelloFS.java
+++ proj/src/main/java/HelloFS.java
@@ -1,5 +1,8 @@
 import java.io.File;
+import java.io.IOException;
 import java.nio.ByteBuffer;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 import net.fusejna.DirectoryFiller;
 import net.fusejna.ErrorCodes;
@@ -12,21 +15,20 @@ import net.fusejna.util.FuseFilesystemAdapterFull;
 // https://github.com/EtiennePerot/fuse-jna/blob/master/src/main/java/net/fusejna/examples/HelloFS.java
 public class HelloFS extends FuseFilesystemAdapterFull {
 
-	public static void main(final String... args) throws FuseException
-	{
-		if (args.length != 1) {
-			System.err.println("Usage: HelloFS <mountpoint>");
-			System.exit(1);
-		}
-		new HelloFS().log(true).mount(args[0]);
+	public static void main(final String... args) throws FuseException, IOException {
+		Path tempDirWithPrefix = Files.createTempDirectory("");
+		System.out.println("HelloFS.main() " + tempDirWithPrefix);
+		ProcessBuilder pb = new ProcessBuilder("open", tempDirWithPrefix.toAbsolutePath().toString());
+		Process p = pb.start();
+
+		new HelloFS().log(true).mount(tempDirWithPrefix.toFile());
 	}
 
 	final String filename = "/hello.txt";
 	final String contents = "Hello World!\n";
 
 	@Override
-	public int getattr(final String path, final StatWrapper stat)
-	{
+	public int getattr(final String path, final StatWrapper stat) {
 		if (path.equals(File.separator)) { // Root directory
 			stat.setMode(NodeType.DIRECTORY);
 			return 0;
@@ -39,8 +41,8 @@ public class HelloFS extends FuseFilesystemAdapterFull {
 	}
 
 	@Override
-	public int read(final String path, final ByteBuffer buffer, final long size, final long offset, final FileInfoWrapper info)
-	{
+	public int read(final String path, final ByteBuffer buffer, final long size, final long offset,
+			final FileInfoWrapper info) {
 		// Compute substring that we are being asked to read
 		final String s = contents.substring((int) offset,
 				(int) Math.max(offset, Math.min(contents.length() - offset, offset + size)));
@@ -49,8 +51,7 @@ public class HelloFS extends FuseFilesystemAdapterFull {
 	}
 
 	@Override
-	public int readdir(final String path, final DirectoryFiller filler)
-	{
+	public int readdir(final String path, final DirectoryFiller filler) {
 		filler.add(filename);
 		return 0;
 	}
