b975237092f6ef6b78c142dc22cee67b3b639b7f
Sridhar Sarnobat <ss401533@gmail.com>
2024-04-01 20:38:06 -0700

    Minor changes (air)

--- list_files.sh
+++ list_files.sh
@@ -3,9 +3,13 @@ cat <<'EOF' | tee /tmp/gedcom_individuals.awk >/dev/null
 $0  ~ /I[0-9]{1,}$/    {
 	PATH=$1
 	gsub(/\//,"",PATH)
-	printf("grep %s /Volumes/git/2023/repos_personal.git/sarnobat.git/2023/genealogy/auto/gedcom_individuals.auto.csv\n", PATH)
+	printf("grep '%s\\b' /Volumes/git/2023/repos_personal.git/sarnobat.git/2023/genealogy/auto/gedcom_individuals.auto.csv\n", PATH)
 }
 
+EOF
+
+cat <<'EOF' | tee /tmp/gedcom_individual_code_to_name.sh >/dev/null
+basename "$1" | awk -f /tmp/gedcom_individuals.awk | tee -a /tmp/gedcom_grep.out | xargs -d'\n' -I% sh -c "%" | perl -pe 's{^[^s]+\s+}{}' 
 EOF
 
 echo "SRIDHAR $@" >&2
@@ -16,4 +20,4 @@ test "$1" = "/by_name" && cat /Volumes/git/2023/repos_personal.git/sarnobat.git/
 # [[ "$1" =~ '/by_name/I[0-9]*$' ]] && echo "$1" | awk -f /tmp/gedcom_individuals.awk | xargs -d'\n' -I% sh -c "%" | perl -pe 's{^[^s]+\s+}{}' | perl -pe 's{\s*$}{.txt}'
 # [[ "$1" =~ '/by_name/I' ]] && echo "$1---"
 
-(echo "$1" | grep -q -P "/by_name/I[0-9]{1,4}$") && (basename "$1" | awk -f /tmp/gedcom_individuals.awk | xargs -d'\n' -I% sh -c "%" | perl -pe 's{^[^s]+\s+}{}' | perl -pe 's{\s*$}{.txt}')
+(echo "$1" | grep -q -P "/by_name/I[0-9]{1,4}$") && (sh /tmp/gedcom_individual_code_to_name.sh "$1" | perl -pe 's{\s*$}{.txt}')
