5f18712c8341b38c3d9dcf75af3d42fc2e3bb924
Sridhar Sarnobat <ss401533@gmail.com>
2024-04-13 01:34:24 -0700

    Minor changes (untested) 2024-04-13

--- run_native.sh
+++ run_native.sh
@@ -1,5 +1,5 @@
 set -e
-
+ 
 GRAALVM_HOME=/Volumes/Apps/graalvm-ce-java11-22.2.0/Contents/Home/
 
-$GRAALVM_HOME/bin/native-image -H:Class=com.App -jar proj/target/myproj-1.1-SNAPSHOT-jar-with-dependencies.jar -H:Name=fusejava.osx
+$GRAALVM_HOME/bin/native-image -H:Class=FuseShellScriptCallouts -jar proj/target/myproj-1.1-SNAPSHOT-jar-with-dependencies.jar -H:Name=fusejava.osx
