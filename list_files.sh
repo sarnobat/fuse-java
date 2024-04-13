f2dab12a999e745ded9652fa7ac362f2a5cec978
Sridhar Sarnobat <ss401533@gmail.com>
2024-04-13 01:06:35 -0700

    Minor changes (untested) 2024-04-13

--- list_files.sh
+++ list_files.sh
@@ -1,3 +1,4 @@
+
 cat <<'EOF' | tee /tmp/gedcom_individuals.awk >/dev/null
 
 $0  ~ /I[0-9]{1,}$/    {
@@ -8,16 +9,22 @@ $0  ~ /I[0-9]{1,}$/    {
 
 EOF
 
-cat <<'EOF' | tee /tmp/gedcom_individual_code_to_name.sh >/dev/null
-basename "$1" | awk -f /tmp/gedcom_individuals.awk | tee -a /tmp/gedcom_grep.out | xargs -d'\n' -I% sh -c "%" | perl -pe 's{^[^s]+\s+}{}' 
-EOF
+# cat <<'EOF' | tee /tmp/gedcom_individual_code_to_name.sh >/dev/null
+# basename "$1" | awk -f /tmp/gedcom_individuals.awk | tee -a /tmp/gedcom_grep.out | xargs -d'\n' -I% sh -c "%" | perl -pe 's{^[^s]+\s+}{}' 
+# EOF
 
 echo "SRIDHAR $@" >&2
-test "$1" = "/" && echo "by_name"
+test "$1" = "/" && echo "by_name\nby_family"
 test "$1" = "/by_name" && cat /Volumes/git/2023/repos_personal.git/sarnobat.git/2023/genealogy/rohidekar.ged | grep INDI | perl -pe 's{0 @(I\d+)@ INDI}{$1}'
 # test "$1" = "/" || grep --no-filename `basename "$1"` /Volumes/git/2023/repos_personal.git/sarnobat.git/2023/genealogy/auto/gedcom_individuals.auto.csv | perl -pe 's{^[^s]+\s+}{}'
 # [[ "$1" =~ /by_name/I ]] && echo "yes.txt"
 # [[ "$1" =~ '/by_name/I[0-9]*$' ]] && echo "$1" | awk -f /tmp/gedcom_individuals.awk | xargs -d'\n' -I% sh -c "%" | perl -pe 's{^[^s]+\s+}{}' | perl -pe 's{\s*$}{.txt}'
 # [[ "$1" =~ '/by_name/I' ]] && echo "$1---"
 
-(echo "$1" | grep -q -P "/by_name/I[0-9]{1,4}$") && (sh /tmp/gedcom_individual_code_to_name.sh "$1" | perl -pe 's{\s*$}{.txt}')
+# (echo "$1" | grep -q -P "/by_name/I[0-9]{1,4}$") && (sh /tmp/gedcom_individual_code_to_name.sh "$1" | perl -pe 's{\s*$}{.txt}')
+(echo "$1" | grep -q -P "/by_name/I[0-9]{1,4}$") && (sh <<'EOF' -s "$1" | perl -pe 's{\s*$}{.txt}'
+basename "$1" | awk -f /tmp/gedcom_individuals.awk | tee -a /tmp/gedcom_grep.out | xargs -d'\n' -I% sh -c "%" | perl -pe 's{^[^s]+\s+}{}' 
+EOF
+)
+
+test "$1" = "/by_family" && echo "the_root"
---
+++ run_native.sh
@@ -0,0 +1,5 @@
+set -e
+
+GRAALVM_HOME=/Volumes/Apps/graalvm-ce-java11-22.2.0/Contents/Home/
+
+$GRAALVM_HOME/bin/native-image -H:Class=com.App -jar proj/target/myproj-1.1-SNAPSHOT-jar-with-dependencies.jar -H:Name=fusejava.osx
